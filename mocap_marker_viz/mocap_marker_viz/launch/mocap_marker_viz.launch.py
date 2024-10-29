fe324744e59eed52b2bb11f577418089c90c865a
Alberto García <albertogarciagomezjacinto@gmail.com>
2024-10-29 15:47:30 +0100

    Modified launcher to comply with autoflake
    Signed-off-by: Alberto García <albertogarciagomezjacinto@gmail.com>

--- mocap_marker_viz/mocap_marker_viz/launch/mocap_marker_viz.launch.py
+++ mocap_marker_viz/mocap_marker_viz/launch/mocap_marker_viz.launch.py
@@ -16,10 +16,11 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 from launch.conditions import UnlessCondition
+from launch.substitutions import LaunchConfiguration
+
 
 
 def generate_launch_description():
